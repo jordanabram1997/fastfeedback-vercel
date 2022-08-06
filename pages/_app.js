080d7233ed2ada0fb159265c6f8fd59a41846181
Jordan Abram <jordan16abram@gmail.com>
2022-08-06 19:36:07 +0100

    fixed error messages

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,16 @@
+import { ThemeProvider, CSSReset } from "@chakra-ui/core";
 import { AuthProvider } from "../lib/auth";
+import theme from "../styles/theme";
 
 const App = ({ Component, pageProps }) => {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <ThemeProvider theme={theme}>
+      <AuthProvider>
+        <CSSReset />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ThemeProvider>
   );
 };
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
 
 export default App;
